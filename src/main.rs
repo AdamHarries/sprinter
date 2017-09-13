341f1e065bd87805c9e4e6115aac3e59600210ca
Adam Harries <adam.harries@ed.ac.uk>
2017-09-13 18:06:26 +0100

    add clap and add some bogus stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate clap;
+
+use std::process::Command;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
     println!("Hello, world!");
+
+    // arg parsing
+    let matches = App::new("sprinter")
+        .version("1.0")
+        .author("Adam Harries <adam.harries@ed.ac.uk>")
+        .about("Run command line programs quickly, with combinations of arguments");
+
+    // run git and find out where we're
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("git rev-parse --verify HEAD")
+        .output()
+        .expect("Failed to run process");
+    println!("Output: {:?}", output);
+
+
 }
