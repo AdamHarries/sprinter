b9fe2bb421d94663280581d458a51f78b150c6b1
Adam Harries <adam.harries@ed.ac.uk>
2017-10-10 18:43:29 +0100

    more changes

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,13 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
-use serde_json::Error;
-
 use std::fs::File;
 use std::io::prelude::*;
 
 use std::process::Command;
 use clap::{Arg, App, SubCommand};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct CmdLineParameter {
-    flag: String, 
-    values: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Configuration {
-    command: String,
-    arguments: Vec<CmdLineParameter>,
-}
+mod parser;
 
 fn main() {
 
@@ -31,12 +19,18 @@ fn main() {
     let matches = App::new("sprinter")
         .version("1.0")
         .author("Adam Harries <adam.harries@ed.ac.uk>")
-        .about("Run command line programs quickly, with combinations of arguments")
-        .arg(Arg::with_name("CONFIG")
-            .help("The configuration file specifying the program + arguments to be run")
-            .takes_value(true)
-            .required(true)
-            .index(1))
+        .about(
+            "Run command line programs quickly, with combinations of arguments",
+        )
+        .arg(
+            Arg::with_name("CONFIG")
+                .help(
+                    "The configuration file specifying the program + arguments to be run",
+                )
+                .takes_value(true)
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
     // get the filename from the argument parser
@@ -45,69 +39,25 @@ fn main() {
 
     // read the file contents into a string
     let mut file = match File::open(filename) {
-        Err(why) => panic!("couldn't open {}: {}", filename,
-                                                   why),
+        Err(why) => panic!("couldn't open {}: {}", filename, why),
         Ok(file) => file,
     };
 
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
-         Err(why) => panic!("couldn't read {}: {}", filename,
-                                                   why),
+        Err(why) => panic!("couldn't read {}: {}", filename, why),
         Ok(_) => println!("{} contains:\n{}", filename, contents),
     };
 
-    // try and parse a command out of the string that we've got
-    let res: Result<Configuration, Error> = serde_json::from_str(&*contents);
-
-    let structure = match res {
-        Err(err) => 
-            panic!("Error while parsing JSON: {}", err), 
-        Ok(clp) => clp,
-    };
-
-    println!("Got struture: {:?}", structure);
-
-    // now we have the structure, call it
-
-    // we build the argument "backwards", starting with a vector of arguments:
-    let mut argv: Vec<String> = Vec::new();
-    argv.push(structure.command);
-    for arg in structure.arguments {
-        argv.push(arg.flag);
-
-        for value in arg.values {
-            argv.push(value);
-        }
-    }
+    let (cmd, argv) = parser::parse_string(&contents);
 
     // get the output from the command, with our arguments, joined
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(argv.join(" "))
-        .output()
-        .expect("Failed to run process");
+    let mut command = Command::new(cmd);
+    for argvv in argv {
+        command.arg(argvv);
+    }
+
+    let output = command.output().expect("Failed to run process");
     println!("Output: {:?}", output);
-
-    // let example_data = r#"{
-    //     "flag": "-i",
-    //     "values": [
-    //         "10", 
-    //         "20"
-    //     ] 
-    // }"#;
-
-    // let res : Result<CmdLineParameter, Error> = serde_json::from_str(example_data);
-
-    // match res {
-    //     Err(err) => 
-    //         println!("Found error: {}", err), 
-    //     Ok(clp) => 
-    //         println!("Got flag: {}, with {} values", clp.flag, clp.values.len()),
-    // }
-
-    
-
-
 
 }
